f5fe2310a32b9de8414b79afafe7d829c95e6b5a
Victor <vx_zz@hotmail.com>
2022-06-09 10:27:22 -0400

    commit tree 246ed5644b5d48b617bc315d0ca50612e224e43b

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,28 +5,60 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearchInput] = useState("")
+
+
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearch(event) {
+    setSearchInput(event.target.value)
+  }
 
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All"){ 
+      return true}
     return item.category === selectedCategory;
+  }).filter((item) => {
+    return item.name.toLowerCase().includes(search.toLowerCase())
+  });
+
+  const itemToDisplay = itemsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
+
+  function onItemFormSubmit(newItem) {
+     setSubmittedData([...submittedData, newItem]);
+  };
+
+  const submittedItem = submittedData.filter((item) => {
+    if (selectedCategory === "All"){ 
+      return true}
+    return item.category === selectedCategory;
+  }).filter((item) => {
+    return item.name.toLowerCase().includes(search.toLowerCase())
+  });
+
+  const submittedItemFiltered = submittedItem.map((item) => {
+    return (
+      <Item key={item.id} name={item.name} category={item.category} />
+    );
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearch} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemToDisplay}
+        {submittedItemFiltered}
       </ul>
     </div>
   );
 }
 
-export default ShoppingList;
+export default ShoppingList
